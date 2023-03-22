952dd8eda678e35d29dc7809350f15c890056f15
sapril <89457926+saprril@users.noreply.github.com>
2023-03-22 21:39:36 +0700

    Create Poligon.java

---
+++ Praktikum4/org/poligon/Poligon.java
@@ -0,0 +1,20 @@
+/* Nama File		: poligon.java
+   Tanggal Pembuatan: 22 Februari 2023
+   Nama Praktikan   : Safril Isnaini
+   NIM Praktikan	: 24060121130057
+   Laboratorium		: B1
+   Deskripsi		: modul program kelas Poligon
+ */
+
+
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi;
+  public void setJumlahSisi(int x){
+    jumlahSisi = x;
+  }
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+}
