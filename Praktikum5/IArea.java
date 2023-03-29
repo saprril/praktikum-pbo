ab5b613ac851bb0d3f02087e43297219512c5551
sapril <89457926+saprril@users.noreply.github.com>
2023-03-29 16:44:13 +0700

    Create IArea.java

---
+++ Praktikum5/IArea.java
@@ -0,0 +1,13 @@
+/* Nama File		: IArea.java
+   Tanggal Pembuatan: 25 Maret 2023
+   Nama Praktikan   : Safril Isnaini
+   Laboratorium		: B1
+   NIM Praktikan	: 24060121130057
+   Deskripsi		: Interface yang mendefinisikan kontrak bahwa setiap 
+                      kelas yang membuat implementasi dari interface ini
+                      harus memiliki cara untuk menghitung luas
+ */
+
+public interface IArea {
+    public double hitungLuas();
+}
